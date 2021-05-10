9d5949bb28cff3684208f5ee14369f65c8dfd109
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-10 07:23:27 -0400

    use the Filesystem Class to Read a Directory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class Post
 {
   public static function find($slug)
   {
-    if(! file_exists($path = __DIR__. "/../resources/posts/{$slug}.html")) {
-      return redirect('/');
+    if(! file_exists($path = resource_path("/posts/{$slug}.html"))) {
+      throw new ModelNotFoundException();
     }
 
     return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
