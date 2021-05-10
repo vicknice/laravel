4cac647c899700ff0bab48904e07622ef8da7353
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-10 19:07:31 -0400

    View all posts by an author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -13,7 +13,7 @@
 
 
       <p>
-        By <a href="">{{ $post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name}}</a>
+        By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name}}</a>
       </p>
 
       <div>
