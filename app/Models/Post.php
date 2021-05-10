c540aa06543cfdc627794b22ba439f02a8fe1239
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-10 16:54:18 -0400

    View all posts by an author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,8 +17,8 @@ class Post extends Model
     }
 
 
-    public function user()
+    public function author()
     {
-      return $this->belongsTo(User::class);
+      return $this->belongsTo(User::class, 'user_id');
     }
 }
