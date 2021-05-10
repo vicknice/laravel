457ca69b0a3da23088708d7b03a093d038e8a17c
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-10 09:22:26 -0400

    Find a Composer Package for Post Metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,46 +10,42 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 class Post
 {
 
-  public $title;
-  public $excerpt;
-  public $date;
-  public $body;
-  public $slug;
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
 
 
-  public function __construct($title, $excerpt, $date, $body, $slug)
-  {
-    $this->title = $title;
-    $this->excerpt = $excerpt;
-    $this->date = $date;
-    $this->body = $body;
-    $this->slug = $slug;
-  }
-
-
-  public static function all()
-  {
-
-    return collect(File::files(resource_path("/posts/")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document) => new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        ));
-  }
-
-  public static function find($slug)
-  {
-    if(! file_exists($path = resource_path("/posts/{$slug}.html"))) {
-      throw new ModelNotFoundException();
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
     }
 
-    return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
 
-  }
+    public static function all()
+    {
+
+        return collect(File::files(resource_path("/posts/")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
+    }
+
+    public static function find($slug)
+    {
+        // of all of the blog posts, find the one with a slug that matches the one that was requested
+
+    }
 
 
 }
