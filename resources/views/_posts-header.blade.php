34b909c8054d1d7a438e77ca3bfeaa8fb393bc65
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-11 06:10:08 -0400

    How to Extract a Dropdown Blade Component

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -36,7 +36,7 @@
                 @foreach ($categories as $category)
                     <x-dropdown-item
                       href="/categories/{{ $category->slug }}"
-                      :active="request()->is('categories/' . $category->slug)"
+                      :active='request()->is("categories/{$category->slug}")'
                     >{{ ucwords($category->name) }}
                     </x-dropdown-item>
                 @endforeach
