9b96e5af16d21178afe84ac82da2992c540b902c
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-10 20:20:55 -0400

    Blade Components and CSS Grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,42 +2,21 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]"/>
 
-        <x-post-featured-card :post="$posts[0]"/>
-
-        <div class="lg:grid lg:grid-cols-2">
-          <x-post-card />
-          <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+            @if ($posts->count() > 1)
+              <div class="lg:grid lg:grid-cols-6">
+                  @foreach ($posts->skip(1) as $post)
+                      <x-post-card
+                          :post="$post"
+                          class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2'}}"
+                      />
+                  @endforeach
+              </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
     </main>
-
-  {{-- @foreach ($posts as $post)
-    <article>
-      <h1>
-        <a href="/posts/{{ $post->slug }}">
-          {!! $post->title !!}
-        </a>
-      </h1>
-
-      <p>
-        <a href="/categories/{{$post->category->slug}}">{{ $post->category->name}}</a>
-      </p>
-
-
-      <p>
-        By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name}}</a>
-      </p>
-
-      <div>
-        {!! $post->excerpt !!}
-      </div>
-    </article>
-
-  @endforeach --}}
 </x-layout>
