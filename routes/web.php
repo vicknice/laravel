c68d60e45d8d0435094de2e6417d00aaf43aa041
Victor Tolbert <victor.tolbert@gmail.com>
2021-05-28 07:33:02 -0400

    Search (the messy way)

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if(request('search')) {
+      $posts
+        ->where('title', 'like', '%' . request('search') . '%')
+        ->orWhere('title', 'like', '%' . request('search') . '%');
+    }
+
     return view('posts', [
-        'posts' =>  Post::latest()->get(),
+        'posts' =>  $posts->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
